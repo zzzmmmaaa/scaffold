9ef7772f9252e242c70ab6b97ca613fe90311c95
‘sleeyzhang’ <zhangmingan111@163.com>
2024-09-27 16:11:07 +0800

    新增domain模块

---
+++ domain/src/main/java/com/sleepy/model/User.java
@@ -0,0 +1,22 @@
+package com.sleepy.model;
+
+
+import lombok.Data;
+
+/**
+ * 用户领域模型
+ */
+@Data
+public class User {
+
+    /**
+     * 姓名
+     */
+    private String name;
+
+    /**
+     * 名称
+     */
+    private Integer age;
+
+}
