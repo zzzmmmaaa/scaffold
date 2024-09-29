fa197e08a1f05be302b69a349f9bb48edf4b278b
‘sleeyzhang’ <zhangmingan111@163.com>
2024-09-29 12:03:48 +0800

    fix请求类型

--- adapter/src/main/java/com/sleepy/adapter/controller/UserController.java
+++ adapter/src/main/java/com/sleepy/adapter/controller/UserController.java
@@ -3,10 +3,7 @@ package com.sleepy.adapter.controller;
 import com.sleepy.domain.model.User;
 import com.sleepy.domain.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -15,7 +12,7 @@ public class UserController {
     @Autowired
     UserService service;
 
-    @PostMapping("findUserById")
+    @GetMapping("findUserById")
     public Mono<User> findUserById(@RequestParam(value = "id", required = false) String id){
         if (id == null) {
             return Mono.error(new IllegalArgumentException("ID must be provided"));
